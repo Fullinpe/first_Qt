91ef1509a5237f9aad1a269e5c016bbe8e5efc28
fullinpe <fullinpe@qq.com>
2020-04-25 15:50:19 +0800

    a commit

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 #include <QApplication>
 #include "widget.h"
 
-int main(int argc, char *argv[])
-{
+using namespace std;
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     Widget w;
     w.show();
-    return a.exec();
+    cout<<"joighbk"<<endl;
+    return QApplication::exec();
 }

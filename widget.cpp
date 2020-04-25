b20bdd99a48221eb07691eda56d442e2c2ffe193
fullinpe <fullinpe@qq.com>
2020-04-25 23:43:58 +0800

    a commit

--- widget.cpp
+++ widget.cpp
@@ -2,16 +2,25 @@
 #include "ui_widget.h"
 
 Widget::Widget(QWidget *parent)
-    : QWidget(parent)
-    , ui(new Ui::Widget)
-{
+        : QWidget(parent), ui(new Ui::Widget) {
 
     ui->setupUi(this);
-//    connect()
+    connect(ui->mbutton, &QPushButton::clicked, this, [=]() {
+                static int x = 0;
+                x++;
+                ui->progressBar->setValue(x);
+            }
+    );
 }
 
-Widget::~Widget()
-{
+Widget::~Widget() {
     delete ui;
+}
+
+void Widget::mslot() {
+    static int x = 0;
+    x++;
+    ui->progressBar->setValue(x);
+    ui->mb2->setHidden((x % 2) == 1);
 }
 
